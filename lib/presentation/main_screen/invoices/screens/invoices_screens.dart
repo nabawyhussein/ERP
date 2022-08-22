83c14c5ea0694b0e048d75e1ae78defdb6a544c3
nabawyhussein <37081451+nabawyhussein@users.noreply.github.com>
2022-08-22 14:14:55 +0200

    PurchaseInvoicesWidget

--- lib/presentation/main_screen/invoices/screens/invoices_screens.dart
+++ lib/presentation/main_screen/invoices/screens/invoices_screens.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:labeeb_front/presentation/common_widget/build_button_widget.dart';
 import 'package:labeeb_front/presentation/common_widget/common_two_text_row_container.dart';
 import 'package:labeeb_front/presentation/common_widget/date_from_to_column_widget.dart';
+import 'package:labeeb_front/presentation/main_screen/invoices/widgets/purchase_Invoices_widget.dart';
 import 'package:labeeb_front/translations/locale_keys.g.dart';
 import '../../../common_widget/buid_app_bar.dart';
 import '../../../common_widget/common_list_tile_row_data_widget.dart';
@@ -94,34 +95,7 @@ class _InvoicesScreenState extends State<InvoicesScreen> with SingleTickerProvid
                     controller: invoicesTabController,
                     children: [
                       // first tab bar view widget
-                      Column(
-                        children: [
-                          Padding(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: AppSize.size(context).height * 0.02,
-                                  horizontal: AppSize.size(context).width * 0.085),
-                              child: Column(
-                                children: [
-                                  const DateFromToColumnWidget(),
-
-                                  SizedBox(
-                                    height: AppSize.size(context).height * 0.03,
-                                  ),
-                                  CommonContainerTwoTextRow(
-                                    firstTxt: LocaleKeys.dailySalesReport.tr(),
-                                    secondTxt:LocaleKeys.finalReport.tr() ,
-                                  ),
-                                  SizedBox(
-                                    height: AppSize.size(context).height * 0.02,
-                                  ),
-
-                                ],
-                              )),
-                          CommonListTileDataWidget(titleTxt: "ATMA",
-                            subTitleTxt: LocaleKeys.lastMove.tr() +" 12/12/2022 ",
-                            statusTxt: ' 3200 '+ LocaleKeys.Creditor.tr(), circleTxt: 'A',),
-                        ],
-                      ),
+                      const PurchaseInvoicesWidget(),
                       Center(
                           child: Text(
                             LocaleKeys.sorryThereAreNoPreviousRequests.tr(),
