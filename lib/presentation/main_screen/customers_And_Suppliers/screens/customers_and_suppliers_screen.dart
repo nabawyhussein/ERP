2475e4633f40d6144c1b0e3bbf7a0185bcb849de
nabawyhussein <37081451+nabawyhussein@users.noreply.github.com>
2022-08-22 14:10:40 +0200

    CustomersWidget

--- lib/presentation/main_screen/customers_And_Suppliers/screens/customers_and_suppliers_screen.dart
+++ lib/presentation/main_screen/customers_And_Suppliers/screens/customers_and_suppliers_screen.dart
@@ -4,6 +4,7 @@ import 'package:labeeb_front/presentation/common_widget/build_button_widget.dart
 import 'package:labeeb_front/presentation/common_widget/common_two_text_row_container.dart';
 import 'package:labeeb_front/presentation/common_widget/creditor_and_debtor_row_widget.dart';
 import 'package:labeeb_front/presentation/common_widget/print_show_search_row_widget.dart';
+import 'package:labeeb_front/presentation/main_screen/customers_And_Suppliers/widgets/customers_widget.dart';
 import 'package:labeeb_front/translations/locale_keys.g.dart';
 import '../../../common_widget/buid_app_bar.dart';
 import '../../../common_widget/common_list_tile_row_data_widget.dart';
@@ -86,30 +87,7 @@ class _CustomersAndSuppliersScreenState extends State<CustomersAndSuppliersScree
                   child: TabBarView(
                     controller: customersAndSuppliersTabController,
                     children: [
-                      Column(
-                        children: [
-                          Padding(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: AppSize.size(context).height * 0.02,
-                                  horizontal: AppSize.size(context).width * 0.085),
-                              child: Column(
-                                children: [
-                                  const CreditorAndDebtorRowWidget(),
-                                  CommonContainerTwoTextRow(
-                                    firstTxt: LocaleKeys.addMovement.tr(),
-                                    secondTxt:LocaleKeys.movementsReport.tr() ,
-                                  ),
-                                  SizedBox(
-                                    height: AppSize.size(context).height * 0.02,
-                                  ),
-                                  const PrintSearchRowIconsWidget(showPrinter: false),
-                                ],
-                              )),
-                          CommonListTileDataWidget(titleTxt: "ATMA",
-                            subTitleTxt: LocaleKeys.lastMove.tr() +" 12/12/2022 ",
-                            statusTxt: ' 3200 '+ LocaleKeys.Creditor.tr(), circleTxt: 'A',),
-                        ],
-                      ),
+                      CustomersWidget(),
                       Center(
                           child: Text(
                             LocaleKeys.sorryThereAreNoPreviousRequests.tr(),
---
+++ lib/presentation/main_screen/customers_And_Suppliers/widgets/customers_widget.dart
@@ -0,0 +1,43 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:labeeb_front/presentation/common_widget/build_button_widget.dart';
+import 'package:labeeb_front/presentation/common_widget/common_two_text_row_container.dart';
+import 'package:labeeb_front/presentation/common_widget/creditor_and_debtor_row_widget.dart';
+import 'package:labeeb_front/presentation/common_widget/print_show_search_row_widget.dart';
+import 'package:labeeb_front/translations/locale_keys.g.dart';
+import '../../../common_widget/buid_app_bar.dart';
+import '../../../common_widget/common_list_tile_row_data_widget.dart';
+import '../../../resources/color_manger.dart';
+import '../../../resources/font_manger.dart';
+import '../../../resources/style_manger.dart';
+class CustomersWidget extends StatelessWidget {
+  const CustomersWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: AppSize.size(context).height * 0.02,
+                horizontal: AppSize.size(context).width * 0.085),
+            child: Column(
+              children: [
+                const CreditorAndDebtorRowWidget(),
+                CommonContainerTwoTextRow(
+                  firstTxt: LocaleKeys.addMovement.tr(),
+                  secondTxt:LocaleKeys.movementsReport.tr() ,
+                ),
+                SizedBox(
+                  height: AppSize.size(context).height * 0.02,
+                ),
+                const PrintSearchRowIconsWidget(showPrinter: false),
+              ],
+            )),
+        CommonListTileDataWidget(titleTxt: "ATMA",
+          subTitleTxt: LocaleKeys.lastMove.tr() +" 12/12/2022 ",
+          statusTxt: ' 3200 '+ LocaleKeys.Creditor.tr(), circleTxt: 'A',),
+      ],
+    );
+  }
+}
